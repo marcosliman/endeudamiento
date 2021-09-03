dc73a10ba5713d7577a170f539db890e13f57628
jamcj2 <jamcmei@hotmail.com>
2021-09-03 11:16:49 -0500

    Carga 2, Amortizaciones

---
+++ endeuda/Controllers/LeasingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using modelo.Models;
+using modelo.Models.Local;
+using modelo.ViewModel;
+namespace tesoreria.Controllers
+{
+    public class LeasingController : Controller
+    {
+        private ErpContext db = new ErpContext();
+        tesoreria.Helper.Seguridad seguridad = System.Web.HttpContext.Current.Session["Seguridad"] as tesoreria.Helper.Seguridad;
+        // GET: Contrato
+        public ActionResult Amortizaciones()
+        {
+            if (seguridad == null)
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            else if (seguridad != null && !seguridad.TienePermiso("Amortizaciones", Helper.TipoAcceso.Acceder))
+            {
+                return RedirectToAction("Inicio", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
