cfed8e443929b39a8c64649096e436fbe146af1d
jamcj2 <jamcmei@hotmail.com>
2021-09-03 15:12:33 -0500

    Cuarta, Se modifico el menú Leasing por Amortizaciones y dentro he colocado Leasing y Otros Creditos, con su botón de generar y exportar a .csv

---
+++ endeuda/Controllers/AmortizacionesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using modelo.Models;
+using modelo.Models.Local;
+using modelo.ViewModel;
+namespace tesoreria.Controllers
+{
+    public class AmortizacionesController : Controller
+    {
+        private ErpContext db = new ErpContext();
+        tesoreria.Helper.Seguridad seguridad = System.Web.HttpContext.Current.Session["Seguridad"] as tesoreria.Helper.Seguridad;
+        // GET: Contrato
+        public ActionResult Leasing()
+        {
+            if (seguridad == null)
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            else if (seguridad != null && !seguridad.TienePermiso("Leasing", Helper.TipoAcceso.Acceder))
+            {
+                return RedirectToAction("Inicio", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult OtrosCreditos()
+        {
+            if (seguridad == null)
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            else if (seguridad != null && !seguridad.TienePermiso("OtrosCreditos", Helper.TipoAcceso.Acceder))
+            {
+                return RedirectToAction("Inicio", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult ModalDetalleLeasing()
+        {
+            if (seguridad == null)
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
