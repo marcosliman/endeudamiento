8503cea3080da842c921d95f4cb51ab26fc2926f
jamcj2 <jamcmei@hotmail.com>
2021-09-22 13:44:19 -0500

    Se agregó Seguros, según reunión con cliente

---
+++ endeuda/Controllers/SeguroController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using modelo.Models;
+using modelo.Models.Local;
+using modelo.ViewModel;
+namespace tesoreria.Controllers
+{
+    public class SeguroController : Controller
+    {
+        private ErpContext db = new ErpContext();
+        tesoreria.Helper.Seguridad seguridad = System.Web.HttpContext.Current.Session["Seguridad"] as tesoreria.Helper.Seguridad;
+        // GET: Contrato
+        public ActionResult RegistrarSeguro()
+        {
+            if (seguridad == null)
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            else if (seguridad != null && !seguridad.TienePermiso("RegistrarSeguro", Helper.TipoAcceso.Acceder))
+            {
+                return RedirectToAction("Inicio", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult SeguroBuscar()
+        {
+            if (seguridad == null)
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            else if (seguridad != null && !seguridad.TienePermiso("SeguroBuscar", Helper.TipoAcceso.Acceder))
+            {
+                return RedirectToAction("Inicio", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult ModalBuscarActivo()
+        {
+            if (seguridad == null)
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
