084d23fff2f6daf55b847236e69b0845bfc1da5c
Robinson Guallimba <robinson.guallimba@gmail.com>
2021-12-28 19:45:17 -0300

    Vista Contrato
    Implementar fucionalidad para visualizacion del contrato

---
+++ modelo/ViewModel/ContratoLogViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace modelo.ViewModel
+{
+    public class ContratoLogViewModel
+    {
+        public int IdContratoLog { get; set; }
+        public int IdContrato { get; set; }
+        public int IdTipoLog { get; set; }
+        public string NombreTipoLog { get; set; }
+        public string NombreLog { get; set; }
+        public int IdUsuarioResgistro { get; set; }
+        public string NombreUsuarioRegistro { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string FechaRegistroStr { get; set; }
+    }
+
+}
