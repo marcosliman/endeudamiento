a32673565e8d0e6921c7ec4daff4a770bf0bd34a
Robinson Guallimba <robinson.guallimba@gmail.com>
2021-12-30 16:15:07 -0300

    update

---
+++ modelo/Models/Local/PolizaActivo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace modelo.Models.Local
+{
+    [Table("PolizaActivo")]
+    public class PolizaActivo
+    {
+        [Key]
+        [Display(Name = "Código PolizaActivo")]
+        public int IdPolizaActivo { get; set; }
+        public int IdPoliza { get; set; }
+        public int IdActivo { get; set; }
+    }
+}
