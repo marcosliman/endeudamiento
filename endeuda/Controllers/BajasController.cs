d00abc2e0ba5d0fb38a9a4694577476ddd7d8006
Robinson Guallimba <robinson.guallimba@dexiplus.cl>
2024-11-08 13:13:51 -0300

    Prototipo baja activos

---
+++ endeuda/Controllers/BajasController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using modelo.Models;
+using modelo.Models.Local;
+using modelo.ViewModel;
+using System.IO;
+namespace tesoreria.Controllers
+{
+    public class BajasController : Controller
+    {
+        private ErpContext db = new ErpContext();
+        tesoreria.Helper.Seguridad seguridad = System.Web.HttpContext.Current.Session["Seguridad"] as tesoreria.Helper.Seguridad;
+        LoginController loginCtrl = new LoginController();
+        #region Solicitud
+ 
+        public ActionResult SolicitudBuscar()
+        {
+            if (seguridad == null)
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            else if (seguridad != null && !seguridad.TienePermiso("SolicitudBaja", Helper.TipoAcceso.Acceder))
+            {
+                return RedirectToAction("Inicio", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult AddSolicitud(int? id)
+        {
+
+            if (seguridad == null)
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            else if (seguridad != null && !seguridad.TienePermiso("SolicitudBaja", Helper.TipoAcceso.Acceder))
+            {
+                return RedirectToAction("Inicio", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult ModalAsociarActivo(int? id)
+        {
+            var licitacionActivo = new LicitacionActivo();
+            licitacionActivo.IdLicitacion = 0;
+
+            return PartialView(licitacionActivo);
+        }
+
+        public ActionResult ModalVerSolicitud(int? id)
+        {
+            var licitacionActivo = new LicitacionActivo();
+
+            return PartialView(licitacionActivo);
+        }
+        #endregion
+
+        public ActionResult Aprobacion()
+        {
+            return View();
+        }
+
+        public ActionResult SolicitudAprobar(int? id)
+        {
+
+            if (seguridad == null)
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            else if (seguridad != null && !seguridad.TienePermiso("AprobacionBaja", Helper.TipoAcceso.Acceder))
+            {
+                return RedirectToAction("Inicio", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Autorizacion()
+        {
+            return View();
+        }
+
+        public ActionResult SolicitudAutorizar(int? id)
+        {
+
+            if (seguridad == null)
+            {
+                return RedirectToAction("LogOut", "Login");
+            }
+            else if (seguridad != null && !seguridad.TienePermiso("AutorizacionBaja", Helper.TipoAcceso.Acceder))
+            {
+                return RedirectToAction("Inicio", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Reporte()
+        {
+            return View();
+        }
+
+    }
+}
